773471499e35d474ae3c1bc75898ccb9b0543eec
xl402 <xl402@cam.ac.uk>
2020-12-27 17:02:58 +0000

    rename variables

--- test/test_pos2probs.py
+++ test/test_pos2probs.py
@@ -12,30 +12,30 @@ def test_check_ranks_are_normalised_raises_on_unnormalised_input():
     assert 'must sum to 6' in str(e)
 
 
-def test_check_positions_are_normalised_raises_on_out_of_bound_input():
+def test_check_ranks_are_normalised_raises_on_out_of_bound_input():
     bad_input = np.array([0.9, 2.1, 3])
     with pytest.raises(AssertionError) as e:
         check_ranks_are_normalised(bad_input)
     assert 'must be 1 < ranks < 3 for all ranks' in str(e)
 
 
-def test_pos2prob_outputs_doubly_stochastic_matrix():
-    positions = np.array([2., 1., 3.])
-    p = ranks2prob(positions)
+def test_ranks2prob_outputs_doubly_stochastic_matrix():
+    ranks = np.array([2., 1., 3.])
+    p = ranks2prob(ranks)
     assert np.allclose(np.sum(p, axis=0), 1.)
     assert np.allclose(np.sum(p, axis=1), 1.)
 
 
-def test_pos2prob_outputs_sums_to_expected_positions():
+def test_ranks2prob_outputs_sums_to_expected_ranks():
     expected_rank = np.array([2., 1., 3.])
     p = ranks2prob(expected_rank)
-    positions = np.vstack([[1, 2, 3]] * 3)
-    assert np.allclose(np.sum(p * positions, axis=1), expected_rank)
+    ranks = np.vstack([[1, 2, 3]] * 3)
+    assert np.allclose(np.sum(p * ranks, axis=1), expected_rank)
 
 
 def test_normalise_rank():
-    unnormalised_positions = np.array([2., 1.7, 1.1])
-    out = normalise_rank(unnormalised_positions)
+    unnormalised_ranks = np.array([2., 1.7, 1.1])
+    out = normalise_rank(unnormalised_ranks)
     check_ranks_are_normalised(out)
     assert np.allclose(out.sum(), 6)
     assert min(out) >= 1 and max(out) <= 3
