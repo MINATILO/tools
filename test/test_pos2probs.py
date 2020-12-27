49d5d7d6e22e753958fe5c416f1d67e594a9647f
xl402 <xl402@cam.ac.uk>
2020-12-27 12:52:16 +0000

    test passes for pos2probs

--- test/test_pos2probs.py
+++ test/test_pos2probs.py
@@ -4,8 +4,16 @@ import pytest
 from tools.pos2prob import check_positions_are_normalised
 
 
-def test_check_positions_are_normalised():
+def test_check_positions_are_normalised_raises_on_unnormalised_input():
     bad_input = np.array([1.2, 1, 3])
     with pytest.raises(AssertionError) as e:
         check_positions_are_normalised(bad_input)
-    assert 'must sum to' in str(e)
+    assert 'must sum to 6' in str(e)
+
+
+def test_check_positions_are_normalised_raises_on_out_of_bound_input():
+    bad_input = np.array([0.9, 2.1, 3])
+    with pytest.raises(AssertionError) as e:
+        check_positions_are_normalised(bad_input)
+    assert 'must be 1 < positions < 3 for all positions' in str(e)
+
